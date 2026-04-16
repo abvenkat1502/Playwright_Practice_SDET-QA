c49b71068ea9c5ec58d5ab9244bd5bd8f9a1146d
Venkatesh <abvenkat1502@gmail.com>
2026-04-16 18:49:37 +0530

    ABV_Added SDET_Tutorial Workouts with 10 Completed With full Code Run

--- tests/SDET_Tutorial/10_Handle_Dynamic_And_Pagination_Web_Tables.spec.ts
+++ tests/SDET_Tutorial/10_Handle_Dynamic_And_Pagination_Web_Tables.spec.ts
@@ -33,22 +33,24 @@ test("Handling Dynamic Table Values",async({page})=>{
 
     //3. Get all values from the table
     let hasMorePage = true;
-    const rows:Locator[] = await page.locator("#example tbody tr").all();
     while(hasMorePage){
-
+        const rows:Locator[] = await page.locator("#example tbody tr").all();
         for (const row of rows) {
-            console.log(await row.innerText());
+            console.log(`${await row.innerText()}`);
         }
 
         const nxtButton = page.locator("button[aria-label='Next']");
         const buttonAttribute : string | null = await nxtButton.getAttribute('class');
-
+        
+        //await page.waitForTimeout(1000);
+        
         if (buttonAttribute?.includes('disabled')) {
             hasMorePage = false;
+            break;
         } else {
-            nxtButton.click();
-            //await page.waitForTimeout(1000);
+           await nxtButton.click();
         }
+
     }
 });
 
@@ -57,9 +59,9 @@ test("Handling Dynamic Table Listing data count",async({page})=>{
     await page.goto("https://datatables.net/examples/basic_init/zero_configuration.html");    
 
     //4. Check the data displays based on the filter
-    const rows:Locator[] = await page.locator("#example tbody tr").all();
     const dropdown:Locator = page.locator("#dt-length-0");
     await dropdown.selectOption('50');
+    const rows:Locator[] = await page.locator("#example tbody tr").all();
     await page.waitForTimeout(5000);
     expect(rows.length).toBe(50);
 });
@@ -71,9 +73,14 @@ test("Handling Dynamic Table - Filter the specific data on the table",async({pag
     //5. Filter the specific data on the table
     const searchArea = page.locator("#dt-search-0");
     await searchArea.fill("Vivian Harrell");
-    const rows:Locator[] = await page.locator("#example tbody tr").all();
 
-    if (rows.length > 0) {
+    await page.waitForTimeout(2000);
+    const rows:Locator[] = await page.locator("#example tbody tr").all();
+    const columns:Locator[] = await page.locator("#example tbody tr td").all();
+    
+    console.log("Length is", rows.length);
+
+if (columns.length > 1) {
         let matchFound=false;
         for (const row of rows) {
             const txt = await row.innerText();
@@ -83,9 +90,9 @@ test("Handling Dynamic Table - Filter the specific data on the table",async({pag
                 break;
             }
         }
-        expect(matchFound).toBe(true); //or
-        //expect(matchFound).toBeTruthy();
-    } else {
-        console.log("Filtered user details is not found");
-    }
+        //expect(matchFound).toBe(true); //or
+        expect(matchFound).toBeTruthy();
+}else{
+    console.log("NO DATA FOUND");
+}
 });
